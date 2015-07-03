864d58ff4aae0546dbddfd722d468b1ec2dfbff9
crjrzm <cr-jzm@hotmail.com>
2015-07-03 12:21:01 -0600

    prueba 1

---
+++ IExamenParcial/Examen_Clases/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Clases
+{
+    public class Class1
+    {
+    }
+}
